d1b442c235d7caf69ecd125f4d32b66c073c1d96
julian maurcio areiza gallego <julian.areizagallego@gmail.com>
2024-11-28 14:49:30 -0500

    cambios

--- obj/Debug/net6.0/ShoppingAPI_Jueves_2024II.AssemblyInfo.cs
+++ obj/Debug/net6.0/ShoppingAPI_Jueves_2024II.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingAPI_Jueves_2024II")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a294ed1fb744b9369641784df82c789cf932205")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cba13de63379c95c8af3cf397bd805115840c13")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingAPI_Jueves_2024II")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingAPI_Jueves_2024II")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
